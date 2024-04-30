9476d02358a67805e0f18d85592340f09c682544
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2024-04-30 06:17:38 -0400

    draw game

--- app/static/game.js
+++ app/static/game.js
@@ -11,10 +11,9 @@ async function getWords(wordLength) {
 }
 
 // select numWords unique words from wordsArray
-function randomWords(numWords, wordsArray) {
+async function randomWords(numWords, wordsArray) {
     const wordsArrayLength = wordsArray.length;
     const toReturn = [];
-    console.log(wordsArrayLength);
     for(let i = 0; i < numWords; i++) {
         toReturn.push(wordsArray[Math.floor(Math.random() * (wordsArrayLength - 1)) + 0]);
     }
@@ -25,14 +24,62 @@ function randomWords(numWords, wordsArray) {
 async function generateGame(wordLength, numWords) {
     // get words as array
     const wordsArray = await getWords(wordLength);
-    console.log(wordsArray);
     // select words for the game
     const gameArray = randomWords(numWords, wordsArray);
     console.log(gameArray);
+    return gameArray;
 }
 
-function main() {
-    generateGame(4, 4);
+async function shuffleArray(letterArray) {
+    for (let i = letterArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [letterArray[i], letterArray[j]] = [letterArray[j], letterArray[i]];
+      }
+}
+
+async function selectTile() {
+    console.log(this.textContent);
+}
+
+async function drawGame(wordLength, numWords, gameArray) {
+    console.log(gameArray);
+    // flatten gameArray into an array of its component letters
+    const letterArray = gameArray.reduce((letters, word) => letters.concat(word.split('')), []);
+    // shuffle letterArray for displaying in the grid
+    shuffleArray(letterArray)
+    console.log(letterArray);
+    const tileGrid = document.createElement('div');
+    tileGrid.classList.add('tileGrid');
+    for(let i = 0; i < numWords; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for(let j = 0; j < wordLength; j++) {
+            const tile = document.createElement('span');
+            tile.appendChild(document.createTextNode(letterArray[i + j]))
+            tile.classList.add('tile');
+            tile.addEventListener('click', selectTile);
+            row.appendChild(tile);
+        }
+        tileGrid.appendChild(row);
+    }
+    document.querySelector('div.game').appendChild(tileGrid);
+    const inputRow = document.createElement('div');
+    inputRow.classList.add('inputRow');
+    for(let i = 0; i < wordLength; i++) {
+        const tile = document.createElement('span');
+        tile.classList.add('inputTile');
+        tile.addEventListener('click', selectTile);
+        inputRow.appendChild(tile);
+        document.querySelector('div.game').appendChild(inputRow);
+    }
+}
+
+async function main() {
+    const wordLength = 4;
+    const numWords = 4;
+    const gameArray = await generateGame(wordLength, numWords);
+    console.log(gameArray);
+    drawGame(wordLength, numWords, gameArray);
 }
 
 document.addEventListener('DOMContentLoaded', main);
