2ea9f89656c36d7738999d28cf5f52e049165cf8
Josckar <jp5936@nyu.edu>
2024-05-01 03:09:16 -0400

    increase total coverage

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,15 +3,22 @@ Test file for app/app.py
 """
 
 import sys
+import os
+from dotenv import load_dotenv
 import pytest
+import pymongo
 import unittest
-import mongomock
+
+load_dotenv()
 
 sys.path.append("app")
 from app import app
 
-# TODO: Implement Mock data base so the tests don't corrupt the data from the real db.
-# Maybe create a collection for solely testing?
+cxn = pymongo.MongoClient(
+    os.getenv("MONGO_URI", "mongodb://localhost:27017/"),
+    tlsAllowInvalidCertificates=True,
+)
+db = cxn[os.getenv("MONGO_DB", "Tests")]  # store a reference to the database
 
 
 class TestAppRoutes:
@@ -68,3 +75,42 @@ class TestAppRoutes:
     def test_error_handling(self):
         response = self.app.get("/nonexistent-route")
         assert response.status_code == 404
+
+    def test_friends_route_post(self):
+        # Add a user "frienduser" to the database
+        self.app.post(
+            "/signup", data={"username": "frienduser", "password": "password"}
+        )
+
+        self.app.post(
+            "/login",
+            data=dict(username="testuser", password="password"),
+            follow_redirects=True,
+        )
+
+        # Send a POST request to /friends with "frienduser" as the target
+        response = self.app.post("/friends", data={"target": "frienduser"})
+        assert response.status_code == 200
+
+        # Check if the friend is added
+        response = self.app.get("/friends")
+        assert b"frienduser" in response.data
+
+    def test_score_route(self):
+        # Log in as testuser and reset score, then POST
+        self.app.post(
+            "/login",
+            data={"username": "testuser", "password": "password"},
+            follow_redirects=True,
+        )
+
+        db.Users.update_one({"username": "testuser"}, {"$set": {"score": 0}})
+
+        response = self.app.post("/scored")
+
+        # Check if the response code is 200
+        assert response.status_code == 200
+
+        # Check if the score is updated
+        user = db.Users.find_one({"username": "testuser"})
+        assert user["score"] == 1
